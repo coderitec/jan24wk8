86bbbece06e7b014d68367a91a3c8c2105a13303
enemuo <francenemuo@gmail.com>
2024-03-06 17:25:40 +0100

    fix: modify the recommendation

--- bmi/bmi.js
+++ bmi/bmi.js
@@ -13,6 +13,11 @@ function bmiStatus(){
     bmi > 25 ? '<strong>BMI status: </strong> Overweight' :
     bmi >= 18.5 ? '<strong>BMI status: </strong> Healthy weight':
      '<strong>BMI status: </strong> Under weight'
+
+    paras[2].innerHTML = bmi >= 30 ? '<strong>Advice: </strong> The best way to treat obesity is to eat a healthy, reduced-calorie diet and exercise regularly. To do this you should: eat a balanced, calorie-controlled diet.' :
+    bmi > 25 ? '<strong>Advice: </strong> Choosing healthier foods (whole grains, fruits and vegetables, healthy fats and protein sources) and beverages. Limiting unhealthy foods (refined grains and sweets, potatoes, red meat, processed meat) and beverages (sugary drinks) Increasing physical activity. Limiting television time, screen time, and other “sit time”' :
+    bmi >= 18.5 ? '<strong>Advice: </strong> Achieving and maintaining a healthy weight includes healthy eating, physical activity, optimal sleep, and stress reduction. Several other factors may also affect weight gain. Healthy eating features a variety of healthy foods.':
+     '<strong>Advice: </strong> It is important to eat a variety of foods that give you the nutrition you need. You should make sure you eat foods with enough energy to help you gain weight, protein to repair your body and build your muscles, and vitamins and minerals to keep your body cells functioning well.'
 }
 
 inputs[1].addEventListener('change', bmiStatus)
